ae22017345a0776f454cecf57a0fa311922ef82c
Aliaksei Karneyeu <aliaksei.karneyeu@openx.com>
2021-06-05 23:39:17 +0200

    one more update on manual job

--- jobs/manual_job.groovy
+++ jobs/manual_job.groovy
@@ -1,37 +1,12 @@
 pipelineJob('manual_job') {
-
-    description('Job that would be driggered manually')
-
-    // triggers {
-    //     githubPush()
-    // }
-
     definition {
         cps {
-            properties {
-                githubProjectUrl('https://github.com/learningdevops-makvaz-com/ci')
-            }
-
-            logRotator {
-                numToKeep(20)
-                daysToKeep(30)
-            }
-
-            scm {
-                git {
-                    remote {
-                        url('https://github.com/learningdevops-makvaz-com/ci')
-                        // credentials('public')
-                    }
-                    branches('*/master')
-                }
-            }
-
-            sandbox()
             script('''
-            sh ' ls -l' 
-            ''')
+	    node(){
+	      sh 'hello'
+	    }
+	    ''')
+            sandbox()
         }
- 
-  }
+    }
 }

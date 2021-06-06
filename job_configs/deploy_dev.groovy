d0732a13d1f23e8aaf481ce9483024732ec9c090
Aliaksei Karneyeu <aliaksei.karneyeu@openx.com>
2021-06-06 11:22:57 +0200

    add deploy_dev pipeline

---
+++ job_configs/deploy_dev.groovy
@@ -0,0 +1,17 @@
+pipelineJob('manual_job') {
+  definition {
+    cpsScm {
+      scm{
+	git('https://github.com/learningdevops-makvaz-com/ci/', 'master')
+	  scriptPath('jobs/deploy_dev.groovy')
+      }
+    }
+  }
+  parameters {
+    gitParam('plugin_version'){
+      description('version of thank-after-post plugin. Get from git tags')
+	sortMode('DESCENDING_SMART')
+	type('TAG')
+    }
+  }
+}

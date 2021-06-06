b6974db4d0b114fb35144a997f3d668031856519
Aliaksei Karneyeu <aliaksei.karneyeu@openx.com>
2021-06-06 10:54:52 +0200

    use other repo as example

--- jobs/manual_job.groovy
+++ jobs/manual_job.groovy
@@ -1,7 +1,8 @@
 node()
 {
     stage('Checkout repository') {
-      git url: 'https://github.com/learningdevops-makvaz-com/phase01_task03'
+      git url: 'https://github.com/danpaldev/phase01_task03'
+      // git url: 'https://github.com/learningdevops-makvaz-com/phase01_task03'
     }
     stage('count number of sudo commands') {
         sh 'grep sudo setup_wordpress.sh|wc -l'

38394c4ce2789eaf131f638d378866c7ee6d2fbd
Aliaksei Karneyeu <aliaksei.karneyeu@openx.com>
2021-06-06 10:50:42 +0200

    set proper url

--- jobs/manual_job.groovy
+++ jobs/manual_job.groovy
@@ -1,7 +1,7 @@
 node()
 {
     stage('Checkout repository') {
-      git url: 'github.com/learningdevops-makvaz-com/phase01_task03'
+      git url: 'https://github.com/learningdevops-makvaz-com/phase01_task03'
     }
     stage('count number of sudo commands') {
         sh 'grep sudo setup_wordpress.sh'

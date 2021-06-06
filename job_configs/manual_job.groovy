266849833aeb83ec4ec40468f7b3df1e7927e846
Aliaksei Karneyeu <aliaksei.karneyeu@openx.com>
2021-06-06 10:37:39 +0200

    fix man job

--- job_configs/manual_job.groovy
+++ job_configs/manual_job.groovy
@@ -1,11 +1,11 @@
 pipelineJob('manual_job') {
     definition {
         cpsScm {
-	    scm(
+	    scm{
 	      git('https://github.com/learningdevops-makvaz-com/ci/', 'master')
 	      // git('github.com/learningdevops-makvaz-com/phase01_task03', 'master)
-	    )
-            scriptPath('jobs/manual_job.groovy')
+	      scriptPath('jobs/manual_job.groovy')
+	    }
         }
     }
 }

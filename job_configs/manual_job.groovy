eebc979a7ae44b3a48937abdba2679cef1e8837e
Aliaksei Karneyeu <aliaksei.karneyeu@openx.com>
2021-06-06 10:35:45 +0200

    hardcode repo

--- job_configs/manual_job.groovy
+++ job_configs/manual_job.groovy
@@ -2,7 +2,7 @@ pipelineJob('manual_job') {
     definition {
         cpsScm {
 	    scm(
-	      git(env.SEED_JOBS_URL, 'master)
+	      git('https://github.com/learningdevops-makvaz-com/ci/', 'master')
 	      // git('github.com/learningdevops-makvaz-com/phase01_task03', 'master)
 	    )
             scriptPath('jobs/manual_job.groovy')

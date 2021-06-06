699cdd3ec638f4e20733acb946f8b7d01a382b8b
Aliaksei Karneyeu <aliaksei.karneyeu@openx.com>
2021-06-06 10:49:45 +0200

    get only code

--- jobs/manual_job.groovy
+++ jobs/manual_job.groovy
@@ -1,12 +1,7 @@
 node()
 {
     stage('Checkout repository') {
-      dir('scripts') {
-	scmVars = checkout scm
-      }
-      dir('code') {
-	git url: 'github.com/learningdevops-makvaz-com/phase01_task03'
-      }
+      git url: 'github.com/learningdevops-makvaz-com/phase01_task03'
     }
     stage('count number of sudo commands') {
         sh 'grep sudo setup_wordpress.sh'

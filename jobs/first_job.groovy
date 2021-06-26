3050f969625f41aa264a9e7c617aec50b4988b87
Dan <danpal@tuta.io>
2021-06-25 20:15:32 -0500

    koko

--- jobs/first_job.groovy
+++ jobs/first_job.groovy
@@ -1,9 +1,17 @@
 OK="ahjdklaeijdks"
+def lastCommiterEmail = sh(returnStdout: true, script: 'git log --format="%ae" | head -1').trim()
+
 node()
 {
   stage('Probando probando'){
     echo OK 
+    // git branch: master
+    //   credentialsId: 
+    //   url: params.git_url
+    // sh(returnStdout: true, script: "git tag --sort version:refname | tail -1").trim()
     git url: params.git_url
-    sh(returnStdout: true, script: "git tag --sort version:refname | tail -1").trim()
+    sh("pwd")
+    sh("ls")
+    // lastCommiterEmail
   }
 }

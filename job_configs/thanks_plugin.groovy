c5ea4811ef42f55c8c8ebe80a53762a74d8f8d09
Dan <danpal@tuta.io>
2021-06-27 14:30:14 -0500

    Created JobDSL and Jenkinsfile for start working with thanks plugin

---
+++ job_configs/thanks_plugin.groovy
@@ -0,0 +1,14 @@
+pipelineJob('thanks_plugin') {
+    definition {
+      cpsScm {
+        scm {
+          git('https://github.com/danpaldev/thank-after-post-plugin', 'master')
+          scriptPath('jobs/thanks_plugin.jenkinsfile')
+          }
+      }
+    }
+    
+    parameters {
+      stringParam('git_url', 'https://github.com/danpaldev/thank-after-post-plugin', 'main repo url')
+    }
+}

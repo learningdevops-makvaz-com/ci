df9c9c9aabe81c3dfbc149e2ec00e0a75cf8d629
Daniel Palacios <danpal@tuta.io>
2021-08-29 22:47:58 -0500

    exp: triggering thanks from external repo

--- job_configs/manual_job.groovy
+++ job_configs/manual_job.groovy
@@ -1,10 +1,11 @@
+/* groovylint-disable-next-line CompileStatic */
 pipelineJob('manual_job') {
     definition {
         cpsScm {
-	    scm{
-	      git('https://github.com/danpaldev/phase02_task04', 'master')
-	      scriptPath('jobs/manual_job.groovy')
-	    }
+        scm {
+          git('https://github.com/danpaldev/phase02_task04', 'master')
+          scriptPath('jobs/manual_job.jenkinsfile')
+        }
         }
     }
 }
--- job_configs/thanks_plugin.groovy
+++ job_configs/thanks_plugin.groovy
@@ -1,14 +1,11 @@
+/* groovylint-disable-next-line CompileStatic */
 pipelineJob('thanks_plugin') {
     definition {
       cpsScm {
         scm {
-          git('https://github.com/danpaldev/phase02_task04', 'master')
-          scriptPath('jobs/thanks_plugin.jenkinsfile')
+          github('danpaldev/thank-after-post-plugin', 'master', 'ssh')
+          scriptPath('thanks_plugin.jenkinsfile')
           }
       }
-    }
-    
-    parameters {
-      stringParam('git_url', 'git@github.com:danpaldev/thank-after-post-plugin.git', 'main repo url')
     }
 }

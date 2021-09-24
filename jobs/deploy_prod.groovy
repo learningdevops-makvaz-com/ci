c56e484f4aa0e7e471a5571a8bc0603d36051f6f
Dan <danpal@tuta.io>
2021-09-23 21:13:30 -0500

    try: to find if there's no dir change

--- jobs/deploy_prod.groovy
+++ jobs/deploy_prod.groovy
@@ -2,5 +2,9 @@ node()
 {
   stage('Trying job') {
     echo "${params.PLUGIN_TAG_VERSION}"
+    sh 'cat docker-compose.yml'
+    //TODO-> Create .prod.env with the prod url: localhost:81,
+    //TODO-> Create Py Script that swaps port 80 for 81 in compose file.
+      // I could modify the existing one that accepts a flag --production, and auto-swapping the port
   }
 }

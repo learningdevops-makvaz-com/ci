5d63c18d40977274488a977fd5cfa6127458b07f
Dan <danpal@tuta.io>
2021-09-23 22:21:26 -0500

    fix: faulty deploy_prod container name

--- jobs/deploy_prod.groovy
+++ jobs/deploy_prod.groovy
@@ -16,7 +16,7 @@ node()
     sh "python3 yaml_modifier.py --prod docker-compose.yml danpaldev/wp-jenkins:${params.PLUGIN_TAG_VERSION}"
     sh 'docker-compose up -d'
     sh '''
-      until docker container exec deploydev_database_1 mysql -P 3306 -u wp_user --password=wp_password --execute="SHOW DATABASES;" | grep "wordpress" ; do
+      until docker container exec deployprod_database_1 mysql -P 3306 -u wp_user --password=wp_password --execute="SHOW DATABASES;" | grep "wordpress" ; do
         >&2 echo "MySQL is unavailable - waiting for it... 😴"
         sleep 3
       done
